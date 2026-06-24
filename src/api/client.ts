4d388d9ce08f366866fdd6ef1f781d7c580603ab
ixambeejatin <jatin.kumar@ixambee.com>
2026-06-24 06:30:36 +0000

    fixed cors error

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL?.replace(/\/$/, "");
+
 const client = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: apiUrl
 });
 
 client.interceptors.request.use((config) => {
